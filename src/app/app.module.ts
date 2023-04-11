c72a4cbac9dcc892e5216ef4199612a0c23b229a
Claudia Marcela <cmgp.extra@gmail.com>
2023-04-11 18:58:31 -0500

    Crear componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { MostrarNegociosComponent } from './components/mostrar-negocios/mostrar-negocios.component';
+import { MostrarServiciosComponent } from './components/mostrar-servicios/mostrar-servicios.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderSimpleComponent } from './components/header-simple/header-simple.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegistroComponent,
+    MostrarNegociosComponent,
+    MostrarServiciosComponent,
+    HeaderComponent,
+    HeaderSimpleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mostrar-negocios/mostrar-negocios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-negocios',
+  templateUrl: './mostrar-negocios.component.html',
+  styleUrls: ['./mostrar-negocios.component.scss']
+})
+export class MostrarNegociosComponent {
+
+}
